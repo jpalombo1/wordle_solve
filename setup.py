f2613e4021670ce142b22a8fd83e72ee90f6d9be
jpalombo1 <jpyanks95@gmail.com>
2023-01-05 01:43:01 -0500

    final

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup  # type: ignore
 setup(
     name="wordle_solve",
     version="1.0",
-    description="Mortgage Calculation and Analysis Package",
+    description="Wordle solver and play assist tool.",
     author="Joseh Palombo",
     packages=["wordle_solve"],
     setup_requires=["numpy", "pandas", "pandas-stubs", "scipy"],
